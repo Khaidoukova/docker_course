d76a3c72bf86b2c0910c987c132fff5c0c31ee1b
Olga Khaidoukova <khaidoukova@inbox.ru>
2023-10-20 17:22:58 +0500

    Added urls, CRUD for habits model, JWT and permissions.

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ from users.apps import UsersConfig
 app_name = UsersConfig.name
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='users')
+router.register(r'', UserViewSet, basename='users')
 
 
 urlpatterns = [
@@ -16,4 +16,4 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-]
+] + router.urls
