d201aa69e95810f124fdc2b1331357ef52f70525
Olga <khaidoukova@inbox.ru>
2023-10-31 22:39:55 +0500

    added environment variables (dotenv)

--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -22,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m&cc4%+_^#a6qg3p5*(-z!**5l$tch#=g60rkph9zk%)4t17$a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -87,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'tracker',
         'USER': 'postgres',
-        'PASSWORD': '320670',
+        'PASSWORD': os.getenv('PASSWORD'),
 
     }
 }
@@ -138,7 +142,7 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/users/'
 
-TG_API = '6842541820:AAEZwV3FAuBevUqGtNU3frLTFtl3pXDO5x0'
+TG_API = os.getenv('TG_API')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
