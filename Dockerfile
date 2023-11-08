21bc0d57f43641fdf8fb9c3e7f1ce160adc00f21
Olga Khaidoukova <khaidoukova@inbox.ru>
2023-11-08 16:28:58 +0500

    docker settings for redis and celery

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3
 
 WORKDIR /code
 
-COPY ./requirements.txt .
+COPY ./requirements.txt /code/
 
 RUN pip install -r requirements.txt
 
--- config/settings.py
+++ config/settings.py
@@ -155,8 +155,8 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379/'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/'
 
 # Флаг отслеживания выполнения задач
 CELERY_TIMEZONE = "Asia/Yekaterinburg"
